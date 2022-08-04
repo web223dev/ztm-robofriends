41ac3acdaf5bf1238e1771c9edc4b69a7fd87e0b
web223dev <ruslanus866@outlook.com>
2022-08-05 01:10:56 +0800

    remove static robot data and fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
   constructor(){
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then( response => response.json())
+      .then( users => this.setState({ robots: users }))
   }
 
   onSearchChange = (e) => {
@@ -21,13 +26,17 @@ class App extends Component {
 
   render(){
     const filteredRobots = this.state.robots.filter( robot => robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase()) );
-    return (
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    )
+    if ( this.state.robots.length === 0){
+      return <h1>Loading...</h1>
+    } else {
+      return (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      )
+    }
   }
 }
 
